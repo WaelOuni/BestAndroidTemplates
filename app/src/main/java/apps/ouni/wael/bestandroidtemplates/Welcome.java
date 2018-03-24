a696b1dbbf279aaa6a42d02c21330bb0d0b364eb
wael <wael.ouni@squeezer-software.com>
2018-03-24 14:54:33 +0100

    init master branch

---
+++ app/src/main/java/apps/ouni/wael/bestandroidtemplates/Welcome.java
@@ -0,0 +1,13 @@
+package apps.ouni.wael.bestandroidtemplates;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
